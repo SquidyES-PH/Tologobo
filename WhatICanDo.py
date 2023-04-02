7a19ea116e420301c562bb44754c9daeef3f3a34
SquidyES-PH <129446087+SquidyES-PH@users.noreply.github.com>
2023-04-02 19:44:05 +0800

    Create WhatICanDo.py

---
+++ WhatICanDo.py
@@ -0,0 +1,3 @@
+print("CONDUCTION: When you touch a hot pan on the stove, heat is transferred from the pan to your hand through conduction.")
+print("CONVECTION: The air conditioning system in a room is a good example of convection. As the cold air from the AC moves through the room, it displaces the warmer air, which rises and creates a convection current.")
+print("RADIATION: Sitting in front of a campfire or a heater is an example of radiation. In this case, the heat is transferred from the source directly to your body through radiation, without any intermediary medium.")
