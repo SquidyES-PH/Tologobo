9ed17ad47b8edf87cbe5aeb201195242655262dc
SquidyES-PH <129446087+SquidyES-PH@users.noreply.github.com>
2023-04-04 04:36:19 +0800

    Create WhatIKnow.py

---
+++ WhatIKnow.py
@@ -0,0 +1,7 @@
+d. keep thermal energy from moving through the walls
+b. metal
+d. hotness or coldness of an object
+c. conduction, convection and radiation
+c. lose thermal energy and slow down
+
+
