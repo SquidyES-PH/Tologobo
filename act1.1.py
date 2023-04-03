e858dc346347d9718e99e19f98051f094ca34d3c
SquidyES-PH <129446087+SquidyES-PH@users.noreply.github.com>
2023-04-04 04:33:53 +0800

    Create act1.1.py

---
+++ act1.1.py
@@ -0,0 +1,4 @@
+print("The circular motion in the diagram is called convection.
+Warm air rises and cold air sinks because warm air is less dense than cold air and therefore, it is lighter and more buoyant.
+Land is warmer during daytime compared to the sea because land has a lower specific heat capacity compared to water, which means it heats up faster with the same amount of energy input.
+Land is cooler during nighttime compared to the sea because water has a higher specific heat capacity compared to land, which means it cools down slower with the same amount of energy output.")
